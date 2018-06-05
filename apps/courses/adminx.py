86e0a0362cf2c181866b49f98254d927c8fd3206
Jiang Haifeng <jianghaifeng@jiagouyun.com>
2018-06-05 08:36:55 +0800

    update

--- apps/courses/adminx.py
+++ apps/courses/adminx.py
@@ -4,6 +4,11 @@
 __Author__ = "HackFun"
 __Date__ = '2017/9/11 8:34'
 
+import os
+import sys
+import uuid
+import xlrd
+
 import xadmin
 
 from .models import Course, Lesson, Video, CourseResource, BannerCourse
@@ -18,6 +23,11 @@ class LessonInline(object):
 
 class CourseResourceInline(object):
     model = CourseResource
+    extra = 0
+
+
+class VideoInline(object):
+    model = Video
     extra = 0
 
 
@@ -89,6 +99,40 @@ class LessonAdmin(object):
     search_fields = ['course', 'name']
     # 过滤器
     list_filter = ['course__name', 'name', 'add_time']
+    list_editable = ['name']  # 定义字段是否可以直接修改
+    inlines = [VideoInline]
+
+    import_excel = True
+
+    def post(self, request, *args, **kwargs):
+        if 'excel' in request.FILES:
+            params = request.FILES
+            exc = params.get('excel')
+            base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            if exc is not None:
+                tmp_file_folder = 'courses/tmp_files'
+
+                filename = os.path.join(base_dir, tmp_file_folder + "/" + str(uuid.uuid4()) + '.xls')
+                with open(filename, 'wb+') as f:
+                    f.writelines(exc.readlines())
+
+                data = xlrd.open_workbook(filename)
+                table = data.sheets()[0]  # 打开第一张表
+                nrows = table.nrows  # 获取表的行数
+                for i in range(nrows):  # 循环逐行打印
+                    try:
+                        line = table.row_values(i)
+                        lesson = Lesson()
+                        course = Course.objects.get(id=int(line[1]))
+                        if course is not None:
+                            lesson.course = course
+                        lesson.name = line[0]
+                        lesson.save()
+                    except Exception as e:
+                        pass
+                if os.path.exists(filename):  # 删除临时文件
+                    os.remove(filename)
+        return super(LessonAdmin, self).post(request, args, kwargs)
 
 
 class VideoAdmin(object):
@@ -98,6 +142,7 @@ class VideoAdmin(object):
     search_fields = ['lesson', 'name']
     # 过滤器
     list_filter = ['lesson', 'name', 'add_time']
+    import_excel = True
 
     def save_models(self):
         obj = self.new_obj
@@ -114,6 +159,52 @@ class VideoAdmin(object):
                 all_lesson_learn_times += one_lesson_learn_times
             course.learn_times = all_lesson_learn_times
             course.save()
+
+    def post(self, request, *args, **kwargs):
+        if 'excel' in request.FILES:
+            params = request.FILES
+            exc = params.get('excel')
+            base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            if exc is not None:
+                tmp_file_folder = 'courses/tmp_files'
+
+                filename = os.path.join(base_dir, tmp_file_folder + "/" + str(uuid.uuid4()) + '.xls')
+                with open(filename, 'wb+') as f:
+                    f.writelines(exc.readlines())
+
+                data = xlrd.open_workbook(filename)
+                table = data.sheets()[0]  # 打开第一张表
+                nrows = table.nrows  # 获取表的行数
+                for i in range(nrows):  # 循环逐行打印
+                    try:
+                        line = table.row_values(i)
+                        name, lesson_id, url, learn_times = line
+                        video = Video()
+                        lesson = Lesson.objects.get(id=int(lesson_id))
+                        if lesson is not None:
+                            video.lesson = lesson
+                        video.name = name
+                        video.url = url
+                        video.learn_times = int(learn_times)
+                        video.save()
+
+                        course = lesson.course
+                        all_lesson = Lesson.objects.filter(course=course).all()
+                        all_lesson_learn_times = 0
+                        for i_lesson in all_lesson:
+                            one_lesson_learn_times = Video.objects.filter(lesson=i_lesson).aggregate(
+                                Sum('learn_times')).get(
+                                'learn_times__sum', 0)
+                            one_lesson_learn_times = one_lesson_learn_times if one_lesson_learn_times is not None else 0
+                            all_lesson_learn_times += one_lesson_learn_times
+                        course.learn_times = all_lesson_learn_times
+                        course.save()
+
+                    except Exception as e:
+                        pass
+                if os.path.exists(filename):  # 删除临时文件
+                    os.remove(filename)
+        return super(VideoAdmin, self).post(request, args, kwargs)
 
 
 class CourseResourceAdmin(object):
